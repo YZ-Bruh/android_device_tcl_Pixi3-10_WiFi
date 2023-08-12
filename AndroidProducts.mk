488a85e60cdb8895a2e5ed5ee716d75a3f894f48
Franklin Kelechi <70432347+kelexine@users.noreply.github.com>
2023-08-12 14:25:40 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,3 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_Pixi3-10_WiFi.mk
 
-COMMON_LUNCH_CHOICES := \
-    omni_Pixi3-10_WiFi-user \
-    omni_Pixi3-10_WiFi-userdebug \
-    omni_Pixi3-10_WiFi-eng
